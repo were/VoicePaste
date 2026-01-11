15d69ba616953fa31c5d6798c1fee7eb0824be96
Jian Weng <jian.weng465@gmail.com>
2026-01-11 11:24:23 +0300

    Initial Commit

---
+++ VoicePaste/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VoicePaste
+//
+//  Created by Jian Weng on 2026/1/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VoicePaste/VoicePasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoicePasteApp.swift
+//  VoicePaste
+//
+//  Created by Jian Weng on 2026/1/11.
+//
+
+import SwiftUI
+
+@main
+struct VoicePasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
